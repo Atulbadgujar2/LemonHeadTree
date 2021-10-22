7d6b4ed1bfe00de9b3d7d80d2be627d3a109581c
Atul Badgujar <atul.badgujar@thinkaicorp.com>
2021-10-22 09:40:55 +0530

    changes

--- src/app/main/fiber-composition/fiber-composition-add/fiber-composition-add.component.ts
+++ src/app/main/fiber-composition/fiber-composition-add/fiber-composition-add.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NotificationAction } from 'app/core/constans/constants';
+import { FiberCompositionRequestModel } from 'app/core/model/fiber-composition/fiber-composition-request-model';
+import { FiberCompositionModel } from 'app/core/model/fiber-composition/fiber-composition.model';
+import { FiberCompositionService } from 'app/core/services/fiber-composition/fiber-composition-service';
+import { ToastNotificationService } from 'app/core/services/toastnotification.service';
 
 @Component({
   selector: 'app-fiber-composition-add',
@@ -7,9 +13,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiberCompositionAddComponent implements OnInit {
 
-  constructor() { }
+ //for modals button
+ public actionsLayout = 'normal';
+ //for submit
+ submitted = false;
+ // output from parent to close Modal 
+ @Output() closeAddFiberCompositionModal = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+ @ViewChild('name') name?: ElementRef;
 
+
+
+ //form Group tittle
+ fiberComposition: FormGroup |any;
+
+ fiberCompositionModalData: FiberCompositionModel = new FiberCompositionModel();
+ fiberCompositionRequestModelData: FiberCompositionRequestModel = new FiberCompositionRequestModel();
+
+ constructor(private formBuilder: FormBuilder, private toastNotificationService: ToastNotificationService,
+  public fiberCompositionService: FiberCompositionService) { }
+
+ ngAfterViewInit(): void {
+   if(this.name)
+   this.name.nativeElement.focus();
+ }
+
+
+ ngOnInit(): void {
+   //form function declare
+   this.addFormControl();
+
+ }
+
+ //form control function
+ addFormControl() {
+   this.fiberComposition = this.formBuilder.group({
+     id: [''],     
+     fibertype: [''],     
+     compositionPercent: [''],
+     paramType: [''],
+     status: [''],
+     remark: [''],
+   })
+ }
+
+ // on save data
+ public addFiberComposition() {
+   debugger;
+   this.submitted = true
+   if (this.fiberComposition.invalid) {
+     return;
+   }
+   const formValue = this.fiberComposition.value;
+  //  this.FiberCompositionModalData.id = formValue.id;
+  //  this.FiberCompositionModalData.status = formValue.status;
+  //  this.FiberCompositionModalData.remark = formValue.remark;
+   this.fiberCompositionModalData.fibertype = formValue.fibertype;
+   this.fiberCompositionModalData.compositionPercent = formValue.compositionPercent;   
+   this.fiberCompositionRequestModelData.params = this.fiberCompositionModalData;
+   this.fiberCompositionRequestModelData.role.push('admin');
+
+   this.fiberCompositionService.addFiberComposition(this.fiberCompositionRequestModelData).subscribe(data => {
+     this.toastNotificationService.success(NotificationAction.AddedSucessfully);
+     this.onCloseFiberComposition(true);
+   })
+  //  this.onCloseFiberComposition(true);
+ }
+ /**
+ *  convenience getter for easy access to form fields
+ * for add task code from
+ */
+ get formProperty() {
+   return this.fiberComposition.controls;
+ }
+ // get formProperty end...
+
+ // close add modal 
+ public onCloseFiberComposition(flag: boolean) {
+   const obj = {
+     flag: flag,
+     data: this.fiberComposition.value
+   }
+
+   this.closeAddFiberCompositionModal.emit(obj);
+
+ }
 }
